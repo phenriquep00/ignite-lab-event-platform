7346aae3bf226113fe2b23dab668a4e54d7d26bd
Pedro Lima <contact.pedrolima@gmail.com>
2022-06-23 15:16:31 -0300

    adicionado componentes e finalizado componente Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,11 @@
 import { gql, useQuery } from "@apollo/client"
-import { JSXElementConstructor, ReactElement, ReactFragment, ReactPortal, useEffect } from "react"
-import { client } from './lib/apollo'
+import { Event } from "../src/pages/Event"
 
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
-interface Lesson {
-  id: string;
-  title: string;
-}
-
-function App(){
-  const { data } = useQuery<{ lessons: Lesson }>(GET_LESSONS_QUERY)
-
-  console.log(data)
-
+function App() {
   return (
-    <ul>
-      {data?.lessons.map((lesson: { title: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined; }) => {
-        return <li key={lesson.id}>{lesson.title}</li>
-      })}
-    </ul>
+    <div>
+      <Event />
+    </div>
   )
 }
 
